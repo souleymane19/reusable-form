88c1db2b18fef248528672a01513441c8f6a7241
souleymane19 <souleymanefafabh@gmail.com>
2024-07-31 23:30:35 +0200

    input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ParentComponentComponent} from "./parent-component/parent-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParentComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
